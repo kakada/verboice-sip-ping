168683310f040991709012ab69d52955d1326904
kakada <kakada.chheang@gmail.com>
2015-11-05 11:10:54 +0700

    fix missing include class

--- ping.rb
+++ ping.rb
@@ -1,11 +1,12 @@
 require "./ami.rb"
 require "./agi.rb"
+require "./call_tester.rb"
 
 AMI_USERNAME = "verboice"
 AMI_PASSWORD = "verboice"
 SIP_CHANNEL = "verboice_39-outbound"
 
 tester = CallTester.new(AMI_USERNAME, AMI_PASSWORD, SIP_CHANNEL)
-tester.test("verboice.com", "17772892962")
+tester.test("verboice.com", "17772892961")
 
 exit(1) if tester.failed
