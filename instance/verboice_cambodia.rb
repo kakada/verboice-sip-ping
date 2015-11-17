eccb37a45bf5d44c2920f3a9ed67b55df0902b50
kakada <kakada.chheang@gmail.com>
2015-11-17 09:48:09 +0700

    add ping for separates instances

---
+++ instance/verboice_cambodia.rb
@@ -0,0 +1,19 @@
+require "../ami.rb"
+require "../agi.rb"
+require "../call_tester.rb"
+require "../settings.rb"
+
+require "nuntium"
+
+tester = CallTester.new(Settings.asterisk_config['ami_username'], Settings.asterisk_config['ami_password'], Settings.asterisk_config['sip_channel'])
+tester.test("verboice-cambodia.instedd.org", "17772644173")
+
+if tester.failed
+  nuntium = Nuntium.new Settings.nuntium_config['host'], Settings.nuntium_config['account'], Settings.nuntium_config['application'], Settings.nuntium_config['password']
+  nuntium.send_ao({
+    to: "sms://#{Settings.config['notify_number']}",
+    body: tester.error_message,
+    suggested_channel: 'smart'
+  })
+  exit(1)
+end
--- instance/verboice_msi.rb
+++ instance/verboice_msi.rb
@@ -1,12 +1,12 @@
-require "./ami.rb"
-require "./agi.rb"
-require "./call_tester.rb"
-require "./settings.rb"
+require "../ami.rb"
+require "../agi.rb"
+require "../call_tester.rb"
+require "../settings.rb"
 
 require "nuntium"
 
 tester = CallTester.new(Settings.asterisk_config['ami_username'], Settings.asterisk_config['ami_password'], Settings.asterisk_config['sip_channel'])
-tester.test("verboice.com", "17772892961")
+tester.test("MSI-110.74.193.186", "17772092786")
 
 if tester.failed
   nuntium = Nuntium.new Settings.nuntium_config['host'], Settings.nuntium_config['account'], Settings.nuntium_config['application'], Settings.nuntium_config['password']
